e479cbb4de190631d799bdc790649227e792c2bf
Eric Wündisch <eric.wuendisch@charite.de>
2025-12-11 15:05:06 +0100

    fix build process

--- env.js
+++ env.js
@@ -3,3 +3,4 @@ window.__ENV__=Object.assign(window.__ENV__||{}, { API_BASE_URL: 'http://localho
 window.__ENV__=Object.assign(window.__ENV__||{}, { AUTHORITY_URL: 'https://ttp-gw.charite.de/realms/ttp' });
 window.__ENV__=Object.assign(window.__ENV__||{}, { AUTHORITY_CLIENT: 'frontend' });
 window.__ENV__=Object.assign(window.__ENV__||{}, { AUTHORITY_REDIRECT_URI: 'https://localhost:5173/' });
+window.__ENV__=Object.assign(window.__ENV__||{}, { AUTHORITY_SILENT_URI: 'https://localhost:5173/silent-renew.html' });

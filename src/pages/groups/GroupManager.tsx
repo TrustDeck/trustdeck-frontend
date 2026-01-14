ede540f7e9a3d143e4fd8224356674b8f2d8930f
Loic <loic.khodarkovsky@charite.de>
2026-01-14 10:15:13 +0100

    fix #41

--- src/pages/groups/GroupManager.tsx
+++ src/pages/groups/GroupManager.tsx
@@ -297,13 +297,7 @@ export default function GroupManager() {
             onHide={() => setVisible(false)}
           >
             <p className="m-0">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+             This page shows all of the groups and subgroups in your project. The first group has been created for you. To create a subgroup for it, click the "New group" button and follow the prompts in the inspector on the right side of the screen. 
             </p>
           </Dialog>
         </div>

e838b02de206198c22a7cab03991975cd444028e
Loic <loic.khodarkovsky@charite.de>
2026-02-05 16:54:56 +0100

    fix #45

--- src/core/components/common/Breadcrumbs.tsx
+++ src/core/components/common/Breadcrumbs.tsx
@@ -24,7 +24,7 @@ const Breadcrumbs: React.FC = () => {
     template: itemTemplate
   }))
 
-  const home: MenuItem = { label: t('home'), url: '/' }
+  const home: MenuItem = { label: t('home'), url: '/search' }
 
   return (
     <BreadCrumb
